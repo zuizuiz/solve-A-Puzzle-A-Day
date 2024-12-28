97cb57b5a5bf645493afc8c0aa47e2cfd7dfe0dc
zuizuiz <zuizuiz>
2024-12-28 18:38:57 +0100

    128bit->64bit

--- main.cpp
+++ main.cpp
@@ -1,107 +1,65 @@
 #include <array>
+#include <numbers>
 #include <string>
 #include <vector>
 #include <map>
 #include <bitset>
 #include <iostream>
 #include <ctime>
+#include <fstream>
+#include <filesystem>
 
 #include "puzzle.h"
 #include "pieces.h"
 
-void insertDays(std::map<std::string, UINT64> &map){
+using namespace std;
+
+void insertDays(map<string, UINT64> &map){
 
 	UINT64 index = (UINT64)1 << 38;
 	for(int day = 1; day <= 31; day++){
-		std::string key = "DAY_" + std::to_string(day);
+		string key = "DAY_" + to_string(day);
 		map.insert({key, index});
 
 		index = day % 7 ? index >> 1 : index >> 2;
 	}
 }
 
-void insertMonths(std::map<std::string, UINT64> &map){
+void insertMonths(map<string, UINT64> &map){
 
 	UINT64 index = (UINT64)1 << 54;
 	for(int month = 1; month <= 12; month++){
-		std::string key = "MONTH_" + std::to_string(month);
+		string key = "MONTH_" + to_string(month);
 		map.insert({key, index});
 
 		index = month % 6 ? index >> 1 : index >> 3;
 	}
 }
 
-void removeCurrentDate(std::map<std::string, UINT64> &map){
+map<string, UINT64> removeDate(map<string, UINT64> map, int day, int month){
 
-	std::time_t now = std::time(nullptr);
-	std::tm tm = *std::localtime(&now);
+	time_t now = time(nullptr);
+	tm tm = *localtime(&now);
 
-	std::string day = "DAY_" + std::to_string(tm.tm_mday);
-	std::string month = "MONTH_" + std::to_string(tm.tm_mon + 1);
+	string day_ = "DAY_" + to_string(day);
+	string month_ = "MONTH_" + to_string(month);
 
-	//std::string month = "MONTH_10";
-	//std::string day = "DAY_6";
+	map.erase(day_);
+	map.erase(month_);
 
-	map.erase(day);
-	map.erase(month);
+	return map;
 }
 
-void setBoard(std::map<std::string, UINT64> map, UINT64 &board){
+UINT64 createBoard(map<string, UINT64> map){
 
-	for(auto const& [key, val] : map){
-		board |= val;
-	}
+	UINT64 board = 0;
+	for(auto const& [key, val] : map) board |= val;
+	return board;
 }
 
-std::bitset<128> to_bitset(UINT64 bitboard){
-
-	std::bitset<128> bitset;
-	bitset |= bitboard >> 64;
-	bitset <<= 64;
-	bitset |= bitboard;
-
-	return bitset;
-}
-
-void print(UINT64 bitboard){
-
-	std::bitset<64> bitset = bitboard;
-
-	std::cout << std::endl;
-	for(int i = 0; i < 8; i++){
-		std::cout << bitset.to_string().substr(i * 8, 8) << std::endl;
-	}
-	std::cout << std::endl;
-}
-
-void print(std::vector<UINT64> history, FILE* file = stdout){
+vector<UINT64> fitPiece(UINT64 piece, UINT64 not_board){
 	
-	std::vector<std::bitset<64>> bitsets;
-	for(auto bitboard: history){
-		bitsets.push_back(std::bitset<64>(bitboard));
-	}
-
-	fprintf(file, "\n");
-	for(int i = 63; i >= 0; i--){
-		char square = 'X';
-		for(int j = 0; j < bitsets.size(); j++){
-			if(bitsets[j][i] == 1){
-				square = codes[j];
-				break;
-			}
-		}
-		fprintf(file, " %c ", square);
-		if(i % 8 == 0){
-			fprintf(file, "\n");
-		}
-	}
-	fprintf(file, "\n");
-	
-}
-
-std::vector<UINT64> fitPiece(UINT64 piece, UINT64 not_board){
-	
-	std::vector<UINT64> fits;
+	vector<UINT64> fits;
 
 	for(int i = 4; i < 54; i++){
 		if((piece & not_board) == 0 && piece != 0){
@@ -113,52 +71,114 @@ std::vector<UINT64> fitPiece(UINT64 piece, UINT64 not_board){
 
 	return fits;
 }
+array<vector<UINT64>, 8> fit(UINT64 board, array<vector<UINT64>, 8> &pieces){
 
-std::pair<UINT64, std::vector<UINT64>> recursive(std::array<std::vector<UINT64>, 8> &fits, int index, UINT64 bitboard, std::vector<UINT64> history, int &counter){
-
-	std::pair<UINT64, std::vector<UINT64>> result = index < 8 ? std::pair((UINT64)0, history) : std::pair(bitboard, history);
-
-	for(int i = 0; i < fits[index].size(); i++){
-		if((bitboard & fits[index][i]) == 0){
-			history.push_back(fits[index][i]);
-			result = recursive(fits, index + 1, bitboard | fits[index][i], history, counter);
-			if(result.first > 0 && history.size() == 8){
-				print(result.second);
-				counter++;
-			}
-			history.pop_back();
-		}
-	}
-
-	return result;
-}
-
-int main(){
-
-	UINT64 board = 0;
-	std::map<std::string, UINT64> map;
-
-	insertDays(map);
-	insertMonths(map);
-	removeCurrentDate(map);
-	setBoard(map, board);
-
-	print(board);
-
-	std::array<std::vector<UINT64>, 8> fits;
-	for(int i = 0; i < PIECES.size(); i++){
-		std::vector<UINT64> pieceFits;
-		for(UINT64 subpiece: PIECES[i]){
-			std::vector<UINT64> subpieceFits = fitPiece(subpiece, ~board);
+	array<vector<UINT64>, 8> fits;
+	
+	for(int i = 0; i < pieces.size(); i++){
+		vector<UINT64> pieceFits;
+		
+		for(UINT64 subpiece: pieces[i]){
+			vector<UINT64> subpieceFits = fitPiece(subpiece, ~board);
 			pieceFits.insert(pieceFits.end(), subpieceFits.begin(), subpieceFits.end());
 		}
 		fits[i] = pieceFits;
 	}
 
+	return fits;
+}
+void solve(
+		int index, UINT64 bitboard, 
+		vector<UINT64> history, 
+		array<vector<UINT64>, 8> &fits,
+		multimap<UINT64, vector<UINT64>> &solutions){
 
-	int counter = 0;
-	recursive(fits, 0, 0, std::vector<UINT64>(), counter);
-	printf("Total: %d\n", counter);
-/*
-*/
+	if(index >= 8){
+		solutions.emplace(bitboard, history);
+		return;
+	}
+
+	for(int i = 0; i < fits[index].size(); i++){
+		if((bitboard & fits[index][i]) == 0){
+			history.push_back(fits[index][i]);
+			solve(index + 1, bitboard | fits[index][i], history, fits, solutions);
+			history.pop_back();
+		}
+	}
+}
+
+void print(UINT64 bitboard){
+
+	bitset<64> bitset = bitboard;
+
+	cout << "\n";
+	for(int i = 0; i < 8; i++){
+		cout << bitset.to_string().substr(i * 8, 8) << "\n";
+	}
+	cout << "\n";
+}
+
+void print(vector<UINT64> history, ostream &os = cout){
+	
+	vector<bitset<64>> bitsets;
+	for(auto bitboard: history){
+		bitsets.push_back(bitset<64>(bitboard));
+	}
+
+	os << "\n";
+	for(int i = 63; i >= 0; i--){
+		char square = 'X';
+		for(int j = 0; j < bitsets.size(); j++){
+			if(bitsets[j][i] == 1){
+				square = pieces::codes[j];
+				break;
+			}
+		}
+		os << " " << square << " ";
+		if(i % 8 == 0) os << "\n";
+	}
+	os << "\n";
+}
+
+void print(map<string, UINT64> map, multimap<UINT64, vector<UINT64>> &solution){
+
+	filesystem::path year = "year";
+	filesystem::create_directory(year);
+
+	for(int i = 1; i <= 12; i++){
+
+		filesystem::path month = "month_" + to_string(i);	
+		filesystem::create_directory(year / month);
+
+		for(int j = 1; j <= 31; j++){
+
+			filesystem::path day = "day_" + to_string(j);	
+			UINT64 board = createBoard(removeDate(map, j, i));
+			ofstream file(year / month / day);
+
+			auto range = solution.equal_range(board);
+			int count = 0;
+			for (auto it = range.first; it != range.second; ++it) {
+				print(it->second, file);
+				count++;
+			}
+			file << "Total: " << count;
+			file.close();
+		}
+	}
+}
+
+int main(){
+	
+	multimap<UINT64, vector<UINT64>> solution;
+	map<string, UINT64> map;
+	
+	insertDays(map);
+	insertMonths(map);
+	UINT64 board = createBoard(map);
+
+	array<vector<UINT64>, 8> fits = fit(board, pieces::pieces);
+	solve(0, 0, vector<UINT64>(), fits, solution);
+
+	print(map, solution);
 }
--- pieces.h
+++ pieces.h
@@ -6,239 +6,241 @@
 
 #include "puzzle.h"
 
-constexpr UINT64 PIECE_A1 = 
-    (UINT64)1 << 18 | (UINT64)1 << 17 | (UINT64)1 << 16 |
-    (UINT64)1 << 10 | 
-    (UINT64)1 << 2;
+namespace pieces {
 
-constexpr UINT64 PIECE_A2 = 
-    (UINT64)1 << 18 | (UINT64)1 << 17 | (UINT64)1 << 16 |
-                                        (UINT64)1 << 8  |
-                                        (UINT64)1 << 0;
-constexpr UINT64 PIECE_A3 = 
-    (UINT64)1 << 18 |
-    (UINT64)1 << 10 | 
-    (UINT64)1 << 2  | (UINT64)1 << 1 | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_A1 = 
+		(UINT64)1 << 18 | (UINT64)1 << 17 | (UINT64)1 << 16 |
+		(UINT64)1 << 10 | 
+		(UINT64)1 << 2;
 
-constexpr UINT64 PIECE_A4 = 
-                                      (UINT64)1 << 16 | 
-                                      (UINT64)1 << 8  |
-    (UINT64)1 << 2 | (UINT64)1 << 1 | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_A2 = 
+		(UINT64)1 << 18 | (UINT64)1 << 17 | (UINT64)1 << 16 |
+											(UINT64)1 << 8  |
+											(UINT64)1 << 0;
+	constexpr UINT64 PIECE_A3 = 
+		(UINT64)1 << 18 |
+		(UINT64)1 << 10 | 
+		(UINT64)1 << 2  | (UINT64)1 << 1 | (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_B1 = 
-    (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 |
-    (UINT64)1 << 2  | (UINT64)1 << 1 | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_A4 = 
+										  (UINT64)1 << 16 | 
+										  (UINT64)1 << 8  |
+		(UINT64)1 << 2 | (UINT64)1 << 1 | (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_B2 = 
-    (UINT64)1 << 17 | (UINT64)1 << 16 | 
-    (UINT64)1 << 9  | (UINT64)1 << 8  |
-    (UINT64)1 << 1  | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_B1 = 
+		(UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 |
+		(UINT64)1 << 2  | (UINT64)1 << 1 | (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_C1 = 
-                                      (UINT64)1 << 9 | (UINT64)1 << 8 |
-    (UINT64)1 << 3 | (UINT64)1 << 2 | (UINT64)1 << 1;
+	constexpr UINT64 PIECE_B2 = 
+		(UINT64)1 << 17 | (UINT64)1 << 16 | 
+		(UINT64)1 << 9  | (UINT64)1 << 8  |
+		(UINT64)1 << 1  | (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_C2 = 
-    (UINT64)1 << 11 | (UINT64)1 << 10 | (UINT64)1 << 9 |
-                                        (UINT64)1 << 1 | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_C1 = 
+										  (UINT64)1 << 9 | (UINT64)1 << 8 |
+		(UINT64)1 << 3 | (UINT64)1 << 2 | (UINT64)1 << 1;
 
-constexpr UINT64 PIECE_C3 = 
-                     (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 |
-    (UINT64)1 << 3 | (UINT64)1 << 2;
+	constexpr UINT64 PIECE_C2 = 
+		(UINT64)1 << 11 | (UINT64)1 << 10 | (UINT64)1 << 9 |
+											(UINT64)1 << 1 | (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_C4 = 
-    (UINT64)1 << 11 | (UINT64)1 << 10 |
-                      (UINT64)1 << 2  | (UINT64)1 << 1 | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_C3 = 
+						 (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 |
+		(UINT64)1 << 3 | (UINT64)1 << 2;
 
-constexpr UINT64 PIECE_C5 = 
-    (UINT64)1 << 25 | 
-    (UINT64)1 << 17 | 
-    (UINT64)1 << 9  | (UINT64)1 << 8 | 
-                      (UINT64)1 << 0;
-constexpr UINT64 PIECE_C6 = 
-                     (UINT64)1 << 24 | 
-                     (UINT64)1 << 16 | 
-    (UINT64)1 << 9 | (UINT64)1 << 8  | 
-    (UINT64)1 << 1;
+	constexpr UINT64 PIECE_C4 = 
+		(UINT64)1 << 11 | (UINT64)1 << 10 |
+						  (UINT64)1 << 2  | (UINT64)1 << 1 | (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_C7 = 
-                      (UINT64)1 << 24 |
-    (UINT64)1 << 17 | (UINT64)1 << 16 | 
-    (UINT64)1 << 9  | 
-    (UINT64)1 << 1;
+	constexpr UINT64 PIECE_C5 = 
+		(UINT64)1 << 25 | 
+		(UINT64)1 << 17 | 
+		(UINT64)1 << 9  | (UINT64)1 << 8 | 
+						  (UINT64)1 << 0;
+	constexpr UINT64 PIECE_C6 = 
+						 (UINT64)1 << 24 | 
+						 (UINT64)1 << 16 | 
+		(UINT64)1 << 9 | (UINT64)1 << 8  | 
+		(UINT64)1 << 1;
 
-constexpr UINT64 PIECE_C8 = 
-    (UINT64)1 << 25 |
-    (UINT64)1 << 17 | (UINT64)1 << 16 | 
-                      (UINT64)1 << 8  | 
-                      (UINT64)1 << 0;
+	constexpr UINT64 PIECE_C7 = 
+						  (UINT64)1 << 24 |
+		(UINT64)1 << 17 | (UINT64)1 << 16 | 
+		(UINT64)1 << 9  | 
+		(UINT64)1 << 1;
 
-constexpr UINT64 PIECE_D1 = 
-    (UINT64)1 << 25 | 
-    (UINT64)1 << 17 | 
-    (UINT64)1 << 9  |
-    (UINT64)1 << 1  | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_C8 = 
+		(UINT64)1 << 25 |
+		(UINT64)1 << 17 | (UINT64)1 << 16 | 
+						  (UINT64)1 << 8  | 
+						  (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_D2 = 
-                     (UINT64)1 << 24 | 
-                     (UINT64)1 << 16 | 
-                     (UINT64)1 << 8  |
-    (UINT64)1 << 1 | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_D1 = 
+		(UINT64)1 << 25 | 
+		(UINT64)1 << 17 | 
+		(UINT64)1 << 9  |
+		(UINT64)1 << 1  | (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_D3 = 
-    (UINT64)1 << 25 | (UINT64)1 << 24 |
-    (UINT64)1 << 17 | 
-    (UINT64)1 << 9  | 
-    (UINT64)1 << 1;
+	constexpr UINT64 PIECE_D2 = 
+						 (UINT64)1 << 24 | 
+						 (UINT64)1 << 16 | 
+						 (UINT64)1 << 8  |
+		(UINT64)1 << 1 | (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_D4 = 
-    (UINT64)1 << 25 | (UINT64)1 << 24 |
-                      (UINT64)1 << 16 |
-                      (UINT64)1 << 8  |
-                      (UINT64)1 << 0;
+	constexpr UINT64 PIECE_D3 = 
+		(UINT64)1 << 25 | (UINT64)1 << 24 |
+		(UINT64)1 << 17 | 
+		(UINT64)1 << 9  | 
+		(UINT64)1 << 1;
 
-constexpr UINT64 PIECE_D5 = 
-    (UINT64)1 << 11 | (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 | 
-                                                         (UINT64)1 << 0;
+	constexpr UINT64 PIECE_D4 = 
+		(UINT64)1 << 25 | (UINT64)1 << 24 |
+						  (UINT64)1 << 16 |
+						  (UINT64)1 << 8  |
+						  (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_D6 = 
-    (UINT64)1 << 11 | (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 | 
-    (UINT64)1 << 3;
+	constexpr UINT64 PIECE_D5 = 
+		(UINT64)1 << 11 | (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 | 
+															 (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_D7 = 
-                                                       (UINT64)1 << 8 |
-    (UINT64)1 << 3 | (UINT64)1 << 2 | (UINT64)1 << 1 | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_D6 = 
+		(UINT64)1 << 11 | (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 | 
+		(UINT64)1 << 3;
 
-constexpr UINT64 PIECE_D8 = 
-    (UINT64)1 << 11 |
-    (UINT64)1 << 3  | (UINT64)1 << 2 | (UINT64)1 << 1 | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_D7 = 
+														   (UINT64)1 << 8 |
+		(UINT64)1 << 3 | (UINT64)1 << 2 | (UINT64)1 << 1 | (UINT64)1 << 0;
 
-
-constexpr UINT64 PIECE_E1 = 
-    (UINT64)1 << 11 | (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 | 
-                      (UINT64)1 << 2;
-
-constexpr UINT64 PIECE_E2 = 
-    (UINT64)1 << 11 | (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 | 
-                                        (UINT64)1 << 1;
-
-constexpr UINT64 PIECE_E3 = 
-                     (UINT64)1 << 10 |
-    (UINT64)1 << 3 | (UINT64)1 << 2  | (UINT64)1 << 1 |(UINT64)1 << 0;
-
-constexpr UINT64 PIECE_E4 = 
-                                      (UINT64)1 << 9 |
-    (UINT64)1 << 3 | (UINT64)1 << 2 | (UINT64)1 << 1  |(UINT64)1 << 0;
-
-constexpr UINT64 PIECE_E5 = 
-    (UINT64)1 << 25 |
-    (UINT64)1 << 17 | (UINT64)1 << 16 |
-    (UINT64)1 << 9  |
-    (UINT64)1 << 1;
-
-constexpr UINT64 PIECE_E6 = 
-                       (UINT64)1 << 24 |
-     (UINT64)1 << 17 | (UINT64)1 << 16 |
-                       (UINT64)1 << 8  |
-                       (UINT64)1 << 0;
-
-constexpr UINT64 PIECE_E7 = 
-                     (UINT64)1 << 24 |
-                     (UINT64)1 << 16 |
-    (UINT64)1 << 9 | (UINT64)1 << 8  |
-                     (UINT64)1 << 0;
-
-constexpr UINT64 PIECE_E8 = 
-    (UINT64)1 << 25 |
-    (UINT64)1 << 17 |
-    (UINT64)1 << 9  | (UINT64)1 << 8 |
-    (UINT64)1 << 1;
-
-constexpr UINT64 PIECE_F1 = 
-    (UINT64)1 << 10                 | (UINT64)1 << 8 |
-    (UINT64)1 << 2 | (UINT64)1 << 1 | (UINT64)1 << 0;
-
-constexpr UINT64 PIECE_F2 = 
-    (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 |
-    (UINT64)1 << 2                   | (UINT64)1 << 0;
-
-constexpr UINT64 PIECE_F3 = 
-    (UINT64)1 << 17 | (UINT64)1 << 16 |
-    (UINT64)1 << 9  |
-    (UINT64)1 << 1  | (UINT64)1 << 0;
-
-constexpr UINT64 PIECE_F4 = 
-    (UINT64)1 << 17 | (UINT64)1 << 16 |
-                      (UINT64)1 << 8  |
-    (UINT64)1 << 1  | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_D8 = 
+		(UINT64)1 << 11 |
+		(UINT64)1 << 3  | (UINT64)1 << 2 | (UINT64)1 << 1 | (UINT64)1 << 0;
 
 
-constexpr UINT64 PIECE_G1 = 
-    (UINT64)1 << 18 | 
-    (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 | 
-                                       (UINT64)1 << 0;
+	constexpr UINT64 PIECE_E1 = 
+		(UINT64)1 << 11 | (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 | 
+						  (UINT64)1 << 2;
 
-constexpr UINT64 PIECE_G2 = 
-                                       (UINT64)1 << 16 | 
-    (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 | 
-    (UINT64)1 << 2;
+	constexpr UINT64 PIECE_E2 = 
+		(UINT64)1 << 11 | (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 | 
+											(UINT64)1 << 1;
 
-constexpr UINT64 PIECE_G3 = 
-    (UINT64)1 << 18 | (UINT64)1 << 17 | 
-                      (UINT64)1 << 9  | 
-                      (UINT64)1 << 1  | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_E3 = 
+						 (UINT64)1 << 10 |
+		(UINT64)1 << 3 | (UINT64)1 << 2  | (UINT64)1 << 1 |(UINT64)1 << 0;
 
-constexpr UINT64 PIECE_G4 = 
-                     (UINT64)1 << 17 | (UINT64)1 << 16 | 
-                     (UINT64)1 << 9  | 
-    (UINT64)1 << 2 | (UINT64)1 << 1;
+	constexpr UINT64 PIECE_E4 = 
+										  (UINT64)1 << 9 |
+		(UINT64)1 << 3 | (UINT64)1 << 2 | (UINT64)1 << 1  |(UINT64)1 << 0;
 
-constexpr UINT64 PIECE_H1 = 
-    (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 |
-                      (UINT64)1 << 1 | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_E5 = 
+		(UINT64)1 << 25 |
+		(UINT64)1 << 17 | (UINT64)1 << 16 |
+		(UINT64)1 << 9  |
+		(UINT64)1 << 1;
 
-constexpr UINT64 PIECE_H2 = 
-                     (UINT64)1 << 9 | (UINT64)1 << 8 |
-    (UINT64)1 << 2 | (UINT64)1 << 1 | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_E6 = 
+						   (UINT64)1 << 24 |
+		 (UINT64)1 << 17 | (UINT64)1 << 16 |
+						   (UINT64)1 << 8  |
+						   (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_H3 = 
-    (UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 |
-    (UINT64)1 << 2  | (UINT64)1 << 1;
+	constexpr UINT64 PIECE_E7 = 
+						 (UINT64)1 << 24 |
+						 (UINT64)1 << 16 |
+		(UINT64)1 << 9 | (UINT64)1 << 8  |
+						 (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_H4 = 
-    (UINT64)1 << 10 | (UINT64)1 << 9 |
-    (UINT64)1 << 2  | (UINT64)1 << 1 | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_E8 = 
+		(UINT64)1 << 25 |
+		(UINT64)1 << 17 |
+		(UINT64)1 << 9  | (UINT64)1 << 8 |
+		(UINT64)1 << 1;
 
-constexpr UINT64 PIECE_H5 = 
-    (UINT64)1 << 17 | 
-    (UINT64)1 << 9  | (UINT64)1 << 8 |
-    (UINT64)1 << 1  | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_F1 = 
+		(UINT64)1 << 10                 | (UINT64)1 << 8 |
+		(UINT64)1 << 2 | (UINT64)1 << 1 | (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_H6 = 
-                      (UINT64)1 << 16 | 
-    (UINT64)1 << 9  | (UINT64)1 << 8  |
-    (UINT64)1 << 1  | (UINT64)1 << 0;
+	constexpr UINT64 PIECE_F2 = 
+		(UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 |
+		(UINT64)1 << 2                   | (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_H7 = 
-    (UINT64)1 << 17 | (UINT64)1 << 16 |
-    (UINT64)1 << 9  | (UINT64)1 << 8  |
-                      (UINT64)1 << 0;
+	constexpr UINT64 PIECE_F3 = 
+		(UINT64)1 << 17 | (UINT64)1 << 16 |
+		(UINT64)1 << 9  |
+		(UINT64)1 << 1  | (UINT64)1 << 0;
 
-constexpr UINT64 PIECE_H8 = 
-    (UINT64)1 << 17 | (UINT64)1 << 16 |
-    (UINT64)1 << 9  | (UINT64)1 << 8 |
-    (UINT64)1 << 1;
+	constexpr UINT64 PIECE_F4 = 
+		(UINT64)1 << 17 | (UINT64)1 << 16 |
+						  (UINT64)1 << 8  |
+		(UINT64)1 << 1  | (UINT64)1 << 0;
 
 
-extern std::vector<UINT64> SUBPIECES_A;
-extern std::vector<UINT64> SUBPIECES_B;
-extern std::vector<UINT64> SUBPIECES_C;
-extern std::vector<UINT64> SUBPIECES_D;
-extern std::vector<UINT64> SUBPIECES_E;
-extern std::vector<UINT64> SUBPIECES_F;
-extern std::vector<UINT64> SUBPIECES_G;
-extern std::vector<UINT64> SUBPIECES_H;
-extern std::array<std::vector<UINT64>, 8> PIECES;
-extern std::array<char, 8> codes;
+	constexpr UINT64 PIECE_G1 = 
+		(UINT64)1 << 18 | 
+		(UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 | 
+										   (UINT64)1 << 0;
 
+	constexpr UINT64 PIECE_G2 = 
+										   (UINT64)1 << 16 | 
+		(UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 | 
+		(UINT64)1 << 2;
+
+	constexpr UINT64 PIECE_G3 = 
+		(UINT64)1 << 18 | (UINT64)1 << 17 | 
+						  (UINT64)1 << 9  | 
+						  (UINT64)1 << 1  | (UINT64)1 << 0;
+
+	constexpr UINT64 PIECE_G4 = 
+						 (UINT64)1 << 17 | (UINT64)1 << 16 | 
+						 (UINT64)1 << 9  | 
+		(UINT64)1 << 2 | (UINT64)1 << 1;
+
+	constexpr UINT64 PIECE_H1 = 
+		(UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 |
+						  (UINT64)1 << 1 | (UINT64)1 << 0;
+
+	constexpr UINT64 PIECE_H2 = 
+						 (UINT64)1 << 9 | (UINT64)1 << 8 |
+		(UINT64)1 << 2 | (UINT64)1 << 1 | (UINT64)1 << 0;
+
+	constexpr UINT64 PIECE_H3 = 
+		(UINT64)1 << 10 | (UINT64)1 << 9 | (UINT64)1 << 8 |
+		(UINT64)1 << 2  | (UINT64)1 << 1;
+
+	constexpr UINT64 PIECE_H4 = 
+		(UINT64)1 << 10 | (UINT64)1 << 9 |
+		(UINT64)1 << 2  | (UINT64)1 << 1 | (UINT64)1 << 0;
+
+	constexpr UINT64 PIECE_H5 = 
+		(UINT64)1 << 17 | 
+		(UINT64)1 << 9  | (UINT64)1 << 8 |
+		(UINT64)1 << 1  | (UINT64)1 << 0;
+
+	constexpr UINT64 PIECE_H6 = 
+						  (UINT64)1 << 16 | 
+		(UINT64)1 << 9  | (UINT64)1 << 8  |
+		(UINT64)1 << 1  | (UINT64)1 << 0;
+
+	constexpr UINT64 PIECE_H7 = 
+		(UINT64)1 << 17 | (UINT64)1 << 16 |
+		(UINT64)1 << 9  | (UINT64)1 << 8  |
+						  (UINT64)1 << 0;
+
+	constexpr UINT64 PIECE_H8 = 
+		(UINT64)1 << 17 | (UINT64)1 << 16 |
+		(UINT64)1 << 9  | (UINT64)1 << 8 |
+		(UINT64)1 << 1;
+
+
+	extern std::vector<UINT64> subpieces_A;
+	extern std::vector<UINT64> subpieces_B;
+	extern std::vector<UINT64> subpieces_C;
+	extern std::vector<UINT64> subpieces_D;
+	extern std::vector<UINT64> subpieces_E;
+	extern std::vector<UINT64> subpieces_F;
+	extern std::vector<UINT64> subpieces_G;
+	extern std::vector<UINT64> subpieces_H;
+	extern std::array<std::vector<UINT64>, 8> pieces;
+	extern std::array<char, 8> codes;
+}
 #endif
