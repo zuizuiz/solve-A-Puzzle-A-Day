ad6247048fb1e56eefd4dc773b3801edb31306ad
zuizuiz <zuizuiz>
2024-12-28 11:25:09 +0100

    128bit -> 64bit

--- main.cpp
+++ main.cpp
@@ -9,29 +9,29 @@
 #include "puzzle.h"
 #include "pieces.h"
 
-void insertDays(std::map<std::string, UINT128> &map){
+void insertDays(std::map<std::string, UINT64> &map){
 
-	UINT128 index = (UINT128)1 << 74;
+	UINT64 index = (UINT64)1 << 38;
 	for(int day = 1; day <= 31; day++){
 		std::string key = "DAY_" + std::to_string(day);
 		map.insert({key, index});
 
-		index = day % 7 ? index >> 1 : index >> 5;
+		index = day % 7 ? index >> 1 : index >> 2;
 	}
 }
 
-void insertMonths(std::map<std::string, UINT128> &map){
+void insertMonths(std::map<std::string, UINT64> &map){
 
-	UINT128 index = (UINT128)1 << 96;
+	UINT64 index = (UINT64)1 << 54;
 	for(int month = 1; month <= 12; month++){
 		std::string key = "MONTH_" + std::to_string(month);
 		map.insert({key, index});
 
-		index = month % 6 ? index >> 1 : index >> 6;
+		index = month % 6 ? index >> 1 : index >> 3;
 	}
 }
 
-void removeCurrentDate(std::map<std::string, UINT128> &map){
+void removeCurrentDate(std::map<std::string, UINT64> &map){
 
 	std::time_t now = std::time(nullptr);
 	std::tm tm = *std::localtime(&now);
@@ -39,18 +39,21 @@ void removeCurrentDate(std::map<std::string, UINT128> &map){
 	std::string day = "DAY_" + std::to_string(tm.tm_mday);
 	std::string month = "MONTH_" + std::to_string(tm.tm_mon + 1);
 
+	//std::string month = "MONTH_10";
+	//std::string day = "DAY_6";
+
 	map.erase(day);
 	map.erase(month);
 }
 
-void setBoard(std::map<std::string, UINT128> map, UINT128 &board){
+void setBoard(std::map<std::string, UINT64> map, UINT64 &board){
 
 	for(auto const& [key, val] : map){
 		board |= val;
 	}
 }
 
-std::bitset<128> to_bitset(UINT128 bitboard){
+std::bitset<128> to_bitset(UINT64 bitboard){
 
 	std::bitset<128> bitset;
 	bitset |= bitboard >> 64;
@@ -60,26 +63,26 @@ std::bitset<128> to_bitset(UINT128 bitboard){
 	return bitset;
 }
 
-void print(UINT128 bitboard){
+void print(UINT64 bitboard){
 
-	std::bitset<128> bitset = to_bitset(bitboard);
+	std::bitset<64> bitset = bitboard;
 
 	std::cout << std::endl;
-	for(int i = 0; i < 11; i++){
-		std::cout << bitset.to_string().substr(i * 11 + 7, 11) << std::endl;
+	for(int i = 0; i < 8; i++){
+		std::cout << bitset.to_string().substr(i * 8, 8) << std::endl;
 	}
 	std::cout << std::endl;
 }
 
-void print(std::vector<UINT128> history, FILE* file = stdout){
+void print(std::vector<UINT64> history, FILE* file = stdout){
 	
-	std::vector<std::bitset<128>> bitsets;
+	std::vector<std::bitset<64>> bitsets;
 	for(auto bitboard: history){
-		bitsets.push_back(to_bitset(bitboard));
+		bitsets.push_back(std::bitset<64>(bitboard));
 	}
 
 	fprintf(file, "\n");
-	for(int i = 120; i >= 0; i--){
+	for(int i = 63; i >= 0; i--){
 		char square = 'X';
 		for(int j = 0; j < bitsets.size(); j++){
 			if(bitsets[j][i] == 1){
@@ -88,7 +91,7 @@ void print(std::vector<UINT128> history, FILE* file = stdout){
 			}
 		}
 		fprintf(file, " %c ", square);
-		if(i % 11 == 0){
+		if(i % 8 == 0){
 			fprintf(file, "\n");
 		}
 	}
@@ -96,11 +99,11 @@ void print(std::vector<UINT128> history, FILE* file = stdout){
 	
 }
 
-std::vector<UINT128> fitPiece(UINT128 piece, UINT128 not_board){
+std::vector<UINT64> fitPiece(UINT64 piece, UINT64 not_board){
 	
-	std::vector<UINT128> fits;
+	std::vector<UINT64> fits;
 
-	for(int i = 0; i < 128; i++){
+	for(int i = 4; i < 54; i++){
 		if((piece & not_board) == 0 && piece != 0){
 			fits.push_back(piece);
 		}
@@ -111,9 +114,9 @@ std::vector<UINT128> fitPiece(UINT128 piece, UINT128 not_board){
 	return fits;
 }
 
-std::pair<UINT128, std::vector<UINT128>> recursive(std::array<std::vector<UINT128>, 8> &fits, int index, UINT128 bitboard, std::vector<UINT128> history, int &counter){
+std::pair<UINT64, std::vector<UINT64>> recursive(std::array<std::vector<UINT64>, 8> &fits, int index, UINT64 bitboard, std::vector<UINT64> history, int &counter){
 
-	std::pair<UINT128, std::vector<UINT128>> result = index < 8 ? std::pair((UINT128)0, history) : std::pair(bitboard, history);
+	std::pair<UINT64, std::vector<UINT64>> result = index < 8 ? std::pair((UINT64)0, history) : std::pair(bitboard, history);
 
 	for(int i = 0; i < fits[index].size(); i++){
 		if((bitboard & fits[index][i]) == 0){
@@ -132,26 +135,30 @@ std::pair<UINT128, std::vector<UINT128>> recursive(std::array<std::vector<UINT12
 
 int main(){
 
-	UINT128 board = 0;
-	std::map<std::string, UINT128> map;
+	UINT64 board = 0;
+	std::map<std::string, UINT64> map;
 
 	insertDays(map);
 	insertMonths(map);
 	removeCurrentDate(map);
 	setBoard(map, board);
 
-	std::array<std::vector<UINT128>, 8> fits;
+	print(board);
 
+	std::array<std::vector<UINT64>, 8> fits;
 	for(int i = 0; i < PIECES.size(); i++){
-		std::vector<UINT128> pieceFits;
-		for(UINT128 subpiece: PIECES[i]){
-			std::vector<UINT128> subpieceFits = fitPiece(subpiece, ~board);
+		std::vector<UINT64> pieceFits;
+		for(UINT64 subpiece: PIECES[i]){
+			std::vector<UINT64> subpieceFits = fitPiece(subpiece, ~board);
 			pieceFits.insert(pieceFits.end(), subpieceFits.begin(), subpieceFits.end());
 		}
 		fits[i] = pieceFits;
 	}
 
+
 	int counter = 0;
-	recursive(fits, 0, 0, std::vector<UINT128>(), counter);
+	recursive(fits, 0, 0, std::vector<UINT64>(), counter);
 	printf("Total: %d\n", counter);
+/*
+*/
 }
--- puzzle.h
+++ puzzle.h
@@ -1,6 +1,9 @@
 #ifndef PUZZLE_H
 #define PUZZLE_H
 
+#include <cstdint>
+
 typedef unsigned __int128 UINT128;
+typedef uint64_t UINT64;
 
 #endif
